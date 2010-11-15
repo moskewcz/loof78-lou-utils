695786c3857022c0705a70fa3298df1c15fcf36e
moskewcz <moskewcz@d10678fd-6a51-19ee-c228-53aaa9fc8b4b>
2010-11-15 21:53:26 +0000

    first pass at complete io support for fcp and ncp city layout formats. the internal format is a simple 21x21 matrix of city object names. a whopping two examples of each format are used for some unit tests, so you can be certain there are no bugs. currently, round-trip conversions are used as a proxy for checking the correctness of cross-format conversions.
    the packaging of this code and internal format details will likely change if/when we want to use 
    this code elsewhere.
    
    loof.

--- trunk/city_util.py
+++ trunk/city_util.py
@@ -1,4 +1,3 @@
-
 ncp_kind_char_map = { "land_city":":", "water_city":";" }
 ncp_char_kind_map = dict((v,k) for k, v in ncp_kind_char_map.iteritems())
 assert( len( ncp_kind_char_map ) == len( ncp_char_kind_map ) )
@@ -98,57 +97,7 @@ fcp_cobj_char_map.update( {
         "building_place_on_ground":"0",
         "building_place_on_water":"0",
         } )
-
-
-#note:
-fcp_water_ex = """W
-.....................
-...0000000.000ZZZZ...
-..0000BB00.000LKLK0..
-.000000B00.0D0ZZZZZZ.
-.CCC0B0BB0.000B0NMMM.
-.00C0B0.......0ZZZZZ.
-.0000B..0000D..0NNNN.
-.AA00..ZKZMZLZ..ZZZZ.
-.0A00.0ZKZMZLZ0.NNNN.
-.0AA0.0ZKZMZLZ0.ZZZZ.
-......0ZKZ0ZLZ0......
-.0000.AZKZMZLZ0.R000.
-.0CC0.AZKZMZLZB.0000.
-.0000..ZKZMZLZ..0000.
-.00000..00C00..00JJJ.
-.A00000.......000T00.
-.AAA00BBB0.00D0000TJ.
-.0000BB000.D000T0000.
-..00000BB0.0000JT00..
-...0000000.0B0JJJ0...
-.....................
-"""
-
-
-ncp_water_ex = """;
-#####################
-###-------#---SSSS###
-##----::--#---ALAL-##
-#------:--#-;-SSSSSS#
-#,,,-:-::-#---:-MDDD#
-#--,-:-#######-SSSSS#
-#----:##----;##-MMMM#
-#..--##SLSDSAS##SSSS#
-#-.--#-SLSDSAS-#MMMM#
-#-..-#-SLSDSAS-#SSSS#
-######-SLSTSAS-######
-#----#.SLSDSAS-#J---#
-#-,,-#.SLSDSAS:#----#
-#----##SLSDSAS##----#
-#-----##--,--##--PPP#
-#.-----#######--_R--#
-#...--:::-#--;-_##RP#
-#----::---#;---R###_#
-##-----::-#----PR####
-###-------#-:-PPP_###
-#####################
-"""
+fcp_url_prefix = "http://www.lou-fcp.co.uk/map.php?map="
 
 # for our 'extened' version of the ncp format we use to store our
 # internal templates, we only need this limited mapping
@@ -205,10 +154,11 @@ ncp_ex_land_template = """:
 #####################
 """
 
-import re
+import re, unittest
 
 class CityLayout( object ):
-
+    city_rows = 21
+    city_cols = 21
     # set the templates to None for bootstrapping parse_ncp_str()
     land_template = None
     water_template = None
@@ -220,19 +170,24 @@ class CityLayout( object ):
 
     def __init__( self ):
         self.kind = None
-        self.mat = [ [None]*21 for i in range(0,21) ]
+        self.mat = [ [None]*self.city_cols for i in range(0,self.city_rows) ]
+
+    def check_len_and_parse_kind( self, err_str, s, expected_len, char_kind_map ):
+        if len(s) != expected_len:
+            raise RuntimeError( "bad city %s str length (whitespace removed) %s, expected %s" 
+                                % (err_str, len(s),expected_len) )
+        self.kind = char_kind_map.get(s[0:1],None)
+        if self.kind is None:
+            raise RuntimeError( "bad fcp city type: %s, expected something in %s" %
+                                (s[0:1],char_kind_map.keys() ) )
+        return CityLayout.get_template_for_kind( self.kind )
 
     def parse_ncp_str( self, ncp_str, ex=False ):
-        template = CityLayout.get_template_for_kind( self.kind )
         ncp_str = re.sub(r'\s', '', ncp_str)
+        ss_strip_res = re.search( r'\[ShareString.*?\]([^[]*)(?:\[/ShareString.*\])?', ncp_str )
+        if ss_strip_res: ncp_str = ss_strip_res.group(1)
         expected_len = 1 + sum( len(row) for row in self.mat )
-        if len(ncp_str) != expected_len:
-            raise RuntimeError( "bad ncp_str length (after removing whitespace) %s, expected %s" 
-                                % (len(ncp_str),expected_len) )
-        self.kind = ncp_char_kind_map.get(ncp_str[0:1],None)
-        if self.kind is None:
-            raise RuntimeError( "bad ncp city type: %s, expected something in %s" %
-                                (ncp_str[0:1],ncp_char_kind_map.keys() ) )
+        template = self.check_len_and_parse_kind( "ncp", ncp_str, expected_len, ncp_char_kind_map )
         pos = 1
         for j,row in enumerate(self.mat):
             for i in xrange(0,len(row)):
@@ -248,16 +203,47 @@ class CityLayout( object ):
             pos += len(row)
         return self # syntax sugar
 
+    # wrt our matrix, the fcp format only has data for things that are
+    # not walls in the templates. we use that fact to calc the correct
+    # length, and later in the iteration to fill in the correct
+    # elements. note that both the land and water templates have walls
+    # in the same places, so we arbitrarily use the land template for
+    # the wall checks.
+    def parse_fcp_str( self, fcp_str ):
+        lt_mat = CityLayout.land_template.mat # used for wall checks
+        fcp_str = re.sub(r'\s|\.', '', fcp_str)
+        url_strip_res = re.search( r'map=(.*)', fcp_str )
+        if url_strip_res: fcp_str = url_strip_res.group(1)
+        expected_len = 1 + sum( sum( cobj != "wall" for cobj in row ) for row in lt_mat )
+        template = self.check_len_and_parse_kind( "fcp", fcp_str, expected_len, fcp_char_kind_map )
+        pos = 1
+        for j,row in enumerate(lt_mat):
+            for i in xrange(0,len(row)):
+                if row[i] != "wall":
+                    cobj = fcp_char_cobj_map.get( fcp_str[pos], None )
+                    if cobj == False: cobj = template.mat[j][i]
+                    if cobj is None:
+                        raise RuntimeError( "bad fcp city object char %s, expected something in %s"
+                                            % (fcp_str[pos],fcp_char_cobj_map.keys()) )
+                    self.mat[j][i] = cobj
+                    pos += 1
+                else:
+                    self.mat[j][i] = row[i]
+        return self # syntax sugar
+
     def get_ncp_str( self, readable = False ):
+        ret = "" if readable else "[ShareString.1.3]"
         nl = "\n" if readable else ""
-        ret = ncp_kind_char_map[self.kind] + nl
+        ret += ncp_kind_char_map[self.kind] + nl
         for row in self.mat:
             ret += "".join( ncp_cobj_char_map[cobj] for cobj in row ) + nl
+        ret += "" if readable else "[/ShareString]"
         return ret
 
     def get_fcp_str( self, readable = False ):
+        ret = "" if readable else fcp_url_prefix
         nl = "\n" if readable else ""
-        ret = fcp_kind_char_map[self.kind] + nl
+        ret += fcp_kind_char_map[self.kind] + nl
         for j,row in enumerate(self.mat):
             for i in xrange(0,len(row)):
                 c = fcp_cobj_char_map[row[i]]
@@ -265,16 +251,93 @@ class CityLayout( object ):
                 ret += c
             ret += nl
         return ret
-
     
-
 # for fcp str handling, we need templates. we generate them from the ncp templates above.
 CityLayout.land_template = CityLayout().parse_ncp_str( ncp_ex_land_template, ex=True )
 CityLayout.water_template = CityLayout().parse_ncp_str( ncp_ex_water_template, ex=True )
 
 
-cl = CityLayout()
-cl.parse_ncp_str( ncp_water_ex )
-print cl.get_ncp_str( readable=True )
-print cl.get_fcp_str( readable=True )
+## begin uint test data
+
+fcp_water_ex = """W
+.....................
+...0000000.000ZZZZ...
+..0000BB00.000LKLK0..
+.000000B00.0D0ZZZZZZ.
+.CCC0B0BB0.000B0NMMM.
+.00C0B0.......0ZZZZZ.
+.0000B..0000D..0NNNN.
+.AA00..ZKZMZLZ..ZZZZ.
+.0A00.0ZKZMZLZ0.NNNN.
+.0AA0.0ZKZMZLZ0.ZZZZ.
+......0ZKZ0ZLZ0......
+.0000.AZKZMZLZ0.R000.
+.0CC0.AZKZMZLZB.0000.
+.0000..ZKZMZLZ..0000.
+.00000..00C00..00JJJ.
+.A00000.......000T00.
+.AAA00BBB0.00D0000TJ.
+.0000BB000.D000T0000.
+..00000BB0.0000JT00..
+...0000000.0B0JJJ0...
+.....................
+"""
+
+fcp_ex_1 = "http://www.lou-fcp.co.uk/map.php?map=W0000ZZ0000AA0B000000000C0000000D0000ZZ00000000000000000000000000CC0OOO00AA00005IID0PPPPPA0000DNOO0PRPRP0000A0II5PPPPPPPPP0AAA0OODP0PRPRPRPA000PPPP0PPPP0000P0PRPRPRPPPPD0000PPPPPPPPPP00000000PRPRP0P00C00000PPPPPPPPP0000000PP00PP000D000C0AA0P0000P000000000000P000000000000AAAAPP00000000000000PP0"
+
+ncp_water_ex = """;
+#####################
+###-------#---SSSS###
+##----::--#---ALAL-##
+#------:--#-;-SSSSSS#
+#,,,-:-::-#---:-MDDD#
+#--,-:-#######-SSSSS#
+#----:##----;##-MMMM#
+#..--##SLSDSAS##SSSS#
+#-.--#-SLSDSAS-#MMMM#
+#-..-#-SLSDSAS-#SSSS#
+######-SLSTSAS-######
+#----#.SLSDSAS-#J---#
+#-,,-#.SLSDSAS:#----#
+#----##SLSDSAS##----#
+#-----##--,--##--PPP#
+#.-----#######--_R--#
+#...--:::-#--;-_##RP#
+#----::---#;---R###_#
+##-----::-#----PR####
+###-------#-:-PPP_###
+#####################
+"""
+ncp_ex_1 = "[ShareString.1.3];########################----SS-#---..-:#####--------#-,------###-;----SS-#---------##---------#-------,,##-CCC--#######..----##1FF;-##BBBBB##.----##;MCC##-BJBJB-##---.##-FF1#BBBBBBBBB#-...##-CC;#B-BJBJBJB#.---#######BBBBTBBBB#######----#B-BJBJBJB#BBB;##----#BBBBBBBBB#B---##----##-BJBJB-##B--,##-----##BBBBB##BBBB-##------#######BB__BB##---;---,-#..-B_##_B##---------#---B_###_###-------.#...BB_#######-------#----BB_########################[/ShareString]"
+
+# these tests are pretty limited due to lack of good input examples
+# that exercise all the differs cobjs and such. however, they're a
+# start. 
+class TestCityLayout(unittest.TestCase):
+    def setUp(self):
+        pass
+    fcp_strs = [ (fcp_water_ex,True), (fcp_ex_1,False) ]
+    ncp_strs = [ (ncp_water_ex,True), (ncp_ex_1,False) ]
+
+    def test_fcp_io(self):
+        for fcp_str,readable in self.fcp_strs:
+            cl = CityLayout().parse_fcp_str( fcp_str )
+            self.assertEqual( fcp_str, cl.get_fcp_str( readable ) )
+    def test_ncp_io(self):
+        for ncp_str,readable in self.ncp_strs:
+            cl = CityLayout().parse_ncp_str( ncp_str )
+            self.assertEqual( ncp_str, cl.get_ncp_str( readable ) )
+    def test_roundtrip_fcp(self):
+        for fcp_str,readable in self.fcp_strs:
+            cl = CityLayout().parse_fcp_str( fcp_str )
+            cl2 = CityLayout().parse_ncp_str( cl.get_ncp_str() )
+            self.assertEqual( fcp_str, cl.get_fcp_str( readable ) )
+    def test_roundtrip_ncp(self):
+        for ncp_str,readable in self.ncp_strs:
+            cl = CityLayout().parse_ncp_str( ncp_str )
+            cl2 = CityLayout().parse_fcp_str( cl.get_fcp_str() )
+            self.assertEqual( ncp_str, cl.get_ncp_str( readable ) )
+
+if __name__ == '__main__':
+    unittest.main()
 
